62478aa3d4017405b58a9d799b99eb42e45ef035
Dulpyanghaobo <2533382191@qq.com>
2020-05-27 23:08:20 +0800

    测试

---
+++ Swift-day001-playgoround/Swift-day001-playgoround/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  Swift-day001-playgoround
+//
+//  Created by 杨皓博 on 2020/5/27.
+//  Copyright © 2020 Clinton. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+func getAge()->Int{
+    return 10
+}
+let age = getAge()
+print(age)
+let name : Int = 10
+let http200Status  = (statusCode : 200,descriptor : 100)
+print("thi status code is \(http200Status.statusCode)")
+print(name)
+
